1214f221f86f85fff8d201674443ca6e8f53a543
UsaaryanByte07 <usaaryan79@gmail.com>
2025-08-10 23:38:37 +0530

    Added Chapter 2-4

---
+++ Programming Challenges/Chapter2/src/Challenge_01.java
@@ -0,0 +1,10 @@
+public class Challenge_01 {
+    public static void main(String[] args) {
+        //1. Print Star pattern of inverted right angle traingle(left aligned).
+        System.out.println("* * * * *");
+        System.out.println("* * * *");
+        System.out.println("* * *");
+        System.out.println("* *");
+        System.out.println("*");
+    }
+}
---
+++ Programming Challenges/Chapter3/src/Challenge_02.java
@@ -0,0 +1,6 @@
+public class Challenge_02 {
+    public static void main(String[] args) {
+        //2. Print Star pattern of right angle traingle(Right aligned) using a single print.
+        System.out.println("*\n* *\n* * *\n* * * *\n* * * * *");
+    }
+}
